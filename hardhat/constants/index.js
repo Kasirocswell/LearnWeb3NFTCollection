b7a4948ec86612e52a1b85f17afc53deb26c149c
Kasi Reeves <Kasirocswell@Rocketmail.com>
2022-09-22 08:40:03 -0700

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xaf64733fA116f4D29d8fb4119cBc72b396d087d3";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
